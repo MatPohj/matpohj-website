5c7f81a2666e4df86d22587c7188cc8f07c6882b
Scrocs <159891127+Scrocs@users.noreply.github.com>
2025-03-06 12:06:39 +0200

    Fix case-sensitivity issue in CSS imports

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import '../../styles/Layout.css';
+import '../../styles/layout.css';
 
 interface LayoutProps {
   children: ReactNode;
